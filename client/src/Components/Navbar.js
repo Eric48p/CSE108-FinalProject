3510deabcbbd2fa7b53e433e2c3b0a6611e6b293
Eric Francisco Ponce <144149203+Eric48p@users.noreply.github.com>
2024-04-23 10:18:55 -0700

    Created Navbar
    - This is the first iteration of the navbar

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,44 @@
+import 'rsuite/Navbar/styles/index.css';
+import 'rsuite/Nav/styles/index.css';
+import "../Styles/Navbar.css"
+import { Navbar , Nav } from 'rsuite';
+
+
+
+export default function NavBar(){
+
+
+  return(
+    <Navbar className="custom-Navbar">
+      <Navbar.Brand href="/" className='custom-Navbar-Logo'>
+        <img src="\Images\shpe-logo-horiz-uc-merced-dkbg.png"></img>
+      </Navbar.Brand>
+      <Nav className='custom-Navbar-Links'>
+        <Nav.Item href='/'>Casa</Nav.Item> 
+        <Nav.Menu title="Affairs">
+          <Nav.Item>Executive Board</Nav.Item>
+          <Nav.Item>Weekly Announcements</Nav.Item>
+          <Nav.Item>Calendario</Nav.Item>
+        </Nav.Menu>
+        <Nav.Menu title="Pillars">
+          <Nav.Item>Academics</Nav.Item>
+          <Nav.Menu title="Familia">
+            <Nav.Item>Manitos Mentorship Program</Nav.Item>
+          </Nav.Menu>
+          <Nav.Item>Outreach</Nav.Item>
+          <Nav.Item>Professionalism</Nav.Item>
+          <Nav.Menu title="Network">
+            <Nav.Item>SHPE National Convention</Nav.Item>
+            <Nav.Item>SHPE-tinas</Nav.Item>
+          </Nav.Menu>
+        </Nav.Menu>
+        <Nav.Item>Join</Nav.Item>
+        <Nav.Item>Alumni</Nav.Item>
+        <Nav.Item>Forums</Nav.Item>
+      </Nav>
+      <Nav pullRight className='custom-Navbar-Login'>
+        <Nav.Item>Login / Sign up</Nav.Item>
+      </Nav>
+    </Navbar>
+  )
+}
---
+++ client/src/Pages/Home.js
@@ -0,0 +1,12 @@
+import NavBar from "../Components/Navbar"
+import "../Styles/Home.css"
+
+function Home() {
+
+
+  return(
+    <NavBar />
+  );
+}
+
+export default Home;
