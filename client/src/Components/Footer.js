989b74cae43cc9ada3736fc1b0c138e1f2655271
Eric Francisco Ponce <144149203+Eric48p@users.noreply.github.com>
2024-04-30 11:32:18 -0700

    Footer

---
+++ client/src/Components/Footer.js
@@ -0,0 +1,18 @@
+import "../Styles/Footer.css"
+
+export default function Footer(){
+
+  return(
+    <div className="footer">
+      <a href="https://www.facebook.com/groups/194908704010085" target="_blank">
+        <img src="/Images/Icons/facebook-blk.png" />
+      </a>
+      <a href="https://www.instagram.com/ucm_shpe/" target="_blank">
+        <img src="/Images/Icons/instagram-blk.png" />
+      </a>
+      <a href="https://www.linkedin.com/company/shpe-at-uc-merced/" target="_blank">
+        <img src="/Images/Icons/linkedin-blk.png" />
+      </a>
+    </div>
+  )
+}
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,11 +1,26 @@
+import Footer from "../Components/Footer";
 import NavBar from "../Components/Navbar"
 import "../Styles/Home.css"
+
+import { Carousel } from 'rsuite';
+import 'rsuite/Carousel/styles/index.css';
 
 function Home() {
 
 
   return(
-    <NavBar />
+    <>
+      <NavBar />
+      <div className="home-body">
+        {/* <Carousel autoplay className="custom-slider" placement='right' shape='bar'>
+          <img src="/Images/Main-Page/Home-1.JPG" />
+          <img src="/Images/Main-Page/Home-2.JPG" />
+          <img src="/Images/Main-Page/Home-3.JPG" />
+          <img src="/Images/Main-Page/Home-4.JPG" />
+        </Carousel> */}
+      </div>
+      <Footer />
+    </>
   );
 }
 
