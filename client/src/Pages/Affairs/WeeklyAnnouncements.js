b2442c1ca685234562fca49c2d7aa0a125caada7
gdiaz38 <gdiaz38@ucmerced.edu>
2024-05-02 11:26:02 -0700

    fixed errors and added to alumni

--- client/src/Pages/Affairs/WeeklyAnnouncements.js
+++ client/src/Pages/Affairs/WeeklyAnnouncements.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavBar from "../../Components/Navbar"; // Import the NavBar component
 
 const WeeklyAnnouncements = () => {
   return (
     <div>
+      <NavBar /> {/* Include the NavBar component */}
       <blockquote className="instagram-media" data-instgrm-permalink="https://www.instagram.com/ucm_shpe/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style={{ background: '#FFF', border: 0, borderRadius: '3px', boxShadow: '0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)', margin: '1px', maxWidth: '540px', minWidth: '326px', padding: 0, width: '99.375%', width: '-webkit-calc(100% - 2px)', width: 'calc(100% - 2px)' }}>
         <div style={{ padding: '16px' }}>
           <a href="https://www.instagram.com/ucm_shpe/?utm_source=ig_embed&amp;utm_campaign=loading" style={{ background: '#FFFFFF', lineHeight: 0, padding: 0, textAlign: 'center', textDecoration: 'none', width: '100%' }} target="_blank">
