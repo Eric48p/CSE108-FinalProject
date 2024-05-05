e0f5957704b57564d7eb294a3179dceca61140cd
gdiaz38 <gdiaz38@ucmerced.edu>
2024-05-04 19:01:47 -0700

    changed names for my alumni css

--- client/src/Pages/Alumni.js
+++ client/src/Pages/Alumni.js
@@ -17,73 +17,73 @@ function Alumni() {
           <p className="description">Our Alumni have reached top companies such as Northrop Grumman, Boeing, Rockwell Collins, etc. Networking is highly encouraged!</p>
         </div>
         <div className="slideshowContainer">
-          <div className="slideshow">
-            <div className="slide">
+          <div className="slideshowA">
+            <div className="slideA">
               <img src="https://media.licdn.com/dms/image/D5603AQHtd6CW_hnFVQ/profile-displayphoto-shrink_400_400/0/1684261633777?e=1720051200&v=beta&t=tMZvlAo_xFdNtvdr-Syy4y3zpDkM1wD7kee2HCrmNwg" alt="Alumni 1" />
-              <div className="slideText">
+              <div className="slideTextA">
                 <div><strong>Brandon Ortega</strong></div>
                 <div>Analyst at General Mills</div>
               </div>
             </div>
-            <div className="slide">
+            <div className="slideA">
               <img src="https://media.licdn.com/dms/image/C4E03AQGoqupC3Dhaxg/profile-displayphoto-shrink_800_800/0/1572352146529?e=1720051200&v=beta&t=DYjtUUg73SLY739BELH5sV3y24L_Oy1FJ-MN8Mb-8BY" alt="Alumni 2" />
-              <div className="slideText">
+              <div className="slideTextA">
                 <div><strong>Marcelino García</strong></div>
                 <div>Manufacturing Engineering Manager at Northrop Grumman</div>
                 </div>
             </div>
-            <div className="slide">
+            <div className="slideA">
               <img src="https://media.licdn.com/dms/image/C5603AQEsN4dJnyr40A/profile-displayphoto-shrink_800_800/0/1660326176619?e=1720051200&v=beta&t=D7z86F-W31oPINRN-PdeuAa6U4BmYoEH_-vgCy7vVfo" alt="Alumni 3" />
-              <div className="slideText">
+              <div className="slideTextA">
                 <div><strong>Valeria Cortez</strong></div>
                 <div>Software Engineer at L3Harris Technologies</div>
               </div>
             </div>
-            <div className="slide">
+            <div className="slideA">
               <img src="https://media.licdn.com/dms/image/C4E03AQH31BBKdY9D4A/profile-displayphoto-shrink_800_800/0/1646427266332?e=1720051200&v=beta&t=d11grAeGH72u-I-xonbFZzyJo0ypSappvd_l7R9Vbmk" alt="Alumni 4" />
-              <div className="slideText">
+              <div className="slideTextA">
                 <div><strong>Marina Medina</strong></div>
                 <div>Mechanical Engineer at Northrop Grumman</div>
               </div>
             </div>
-            <div className="slide">
+            <div className="slideA">
               <img src="https://media.licdn.com/dms/image/D5603AQFcBpxknEmOfg/profile-displayphoto-shrink_400_400/0/1701408583566?e=1720051200&v=beta&t=8Zbkh88uKx4jySv1ewD3Nh1qIRpoFWTG64FU4Gc9XKk" alt="Alumni 4" />
-              <div className="slideText">
+              <div className="slideTextA">
                 <div><strong>Christian Montes</strong></div>
                 <div>Systems Engineer at Northrop Grumman</div>
               </div>
             </div>
-            <div className="slide">
+            <div className="slideA">
               <img src="https://media.licdn.com/dms/image/D5603AQGnt-qdB_cuUQ/profile-displayphoto-shrink_400_400/0/1709072257804?e=1720051200&v=beta&t=o--o4dNknP1KGekfcOy9UFCJh7GL3w1X7JZrttNlISA" alt="Alumni 4" />
-              <div className="slideText">
+              <div className="slideTextA">
                 <div><strong>Denise Rosas</strong></div>
                 <div>Quality Engineer at Altec</div>
               </div>
             </div>
-            <div className="slide">
+            <div className="slideA">
               <img src="https://media.licdn.com/dms/image/C5603AQEma7zdlQ36Xg/profile-displayphoto-shrink_400_400/0/1617315411291?e=1720051200&v=beta&t=QcJGUxejbndDSHCeRsPAGOsiAGbXsZFQJKRGUfhf7-c" alt="Alumni 4" />
-              <div className="slideText">
+              <div className="slideTextA">
                 <div><strong>Linda Singh</strong></div>
                 <div>Environmental Technician at (DBA) TOMA-TEK</div>
               </div>
             </div>
-            <div className="slide">
+            <div className="slideA">
               <img src="https://media.licdn.com/dms/image/D5603AQHrXyeTbq4GGQ/profile-displayphoto-shrink_400_400/0/1708981186045?e=1720051200&v=beta&t=OyLtDVJv_0JO8tljELUSNyNCeBZr82JDUBGt-G49Wrs" alt="Alumni 4" />
-              <div className="slideText">
+              <div className="slideTextA">
                 <div><strong>Christopher Patzan</strong></div>
                 <div>Electrical Engineer at Northrop Grumman </div>
               </div>
             </div>
-            <div className="slide">
+            <div className="slideA">
               <img src="https://media.licdn.com/dms/image/D4E03AQFJGfkSLlypDg/profile-displayphoto-shrink_400_400/0/1692132708683?e=1720051200&v=beta&t=hhvroShBo9N-rWHXT-g-Wxy5ulcq_t-h9sBIvN5fSbc" alt="Alumni 4" />
-              <div className="slideText">
+              <div className="slideTextA">
                 <div><strong>Emily Junez</strong></div>
                 <div>Advanced Materials Technology Engineer at General Motors</div>
               </div>
             </div>
-            <div className="slide">
+            <div className="slideA">
               <img src="https://media.licdn.com/dms/image/D5603AQEur3-RLmwXVQ/profile-displayphoto-shrink_400_400/0/1690304564982?e=1720051200&v=beta&t=Iq7J_fXicapkxbY3S98BdBxTyeGWInaTAN_MpIydDcU" alt="Alumni 4" />
-              <div className="slideText">
+              <div className="slideTextA">
                 <div><strong>Betsy Avila</strong></div>
                 <div>Software Engineer at Accenture</div>
               </div>
